c42c23e0f6bfa02cdcff2033a89143e997a669e0
Клюкинк <@227_11>
2018-06-28 09:44:33 +0600

    Add project files.

---
+++ LW_20_minutes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LW_20_minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            task2();
+        }
+
+        /// <summary>
+        /// Даны два целых положительных числа произвольной длины (стоящие из любого количества цифр)
+        /// </summary>
+        static ulong task1()
+        {
+            //1234567889012345678890123457
+            //68890123456788901234567889012
+
+            ulong a = 0;
+            if (UInt64.TryParse(Console.ReadLine(), out a))
+            {
+                ulong ba = 0;
+                if (UInt64.TryParse(Console.ReadLine(), out ba))
+                    return a + ba;
+            }
+
+            return 0;
+        }
+
+        static void task2()
+        {
+            ulong a = UInt64.Parse(Console.ReadLine());
+            ulong s = 0;
+            while (a > 0)
+            {
+                s = s + a % 10;
+                a = a / 10;
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ LW_20_minutes/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LW_20_minutes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LW_20_minutes")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("45d6ac6a-7bc9-428f-b4d5-6691bfb17943")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
